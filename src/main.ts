2f4bac22efcd28cd8fcb3e79b07c76961aff08f1
eledir <eleonora.di.ruscio@gmail.com>
2024-12-18 18:17:08 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// https://v17.angular.io/guide/dynamic-component-loader
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AdBannerComponent } from './app/ad-banner.component';
+
+bootstrapApplication(AdBannerComponent);
